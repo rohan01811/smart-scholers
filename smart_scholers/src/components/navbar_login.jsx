59b8c4758aa22dcf35e145c2d2aef280b589af6c
Siddharth1124 <169067304+Siddharth1124@users.noreply.github.com>
2025-04-09 20:16:26 +0530

    Add files via upload

---
+++ smart_scholers/src/components/navbar_login.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./navbar.css"
+import { NavLink } from "react-router-dom";
+
+function Navbar1() {
+
+    return (
+        <div className="navbar">
+            <div className="name">
+            SMART SCHOLARS
+            </div>
+
+            <div className="log_buttons">
+                <NavLink to={"/"}><button>Home</button></NavLink>
+                <NavLink to={"/signup"}><button>SignUp</button></NavLink>
+            </div>
+            
+        </ div>
+    )
+}
+
+export default Navbar1;
